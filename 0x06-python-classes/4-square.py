78bd3408b2030cd2e0719d8537eab9045c84a6da
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-10-24 17:08:50 -0700

    0x06-python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-""" A class representing a square. """
+""" creates class Square """
 
 
 class Square:
-    """
-    This class allows you to create and manipulate square objects.
-    Each square is defined by its side length.
-    """
-
+    """ Square class"""
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.__size = size
-
-    def area(self):
-        square_area = self.__size * self.__size
-        return (square_area)
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """
-        Retrieve the size (side length) of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return (self.__size)
+        return self.__size
 
     @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
+    def area(self):
+        return self.__size * self.__size
