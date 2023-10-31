0fd93e501433d871a06be556214f58cae25eb686
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-10-31 08:29:04 -0700

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-""" Locked Class  """
+""" Locked class """
 
 
-class LockedClass:
-    """ Locked Class  """
+class LockedClass():
+    """No attribute creation unless attribute = firs_name"""
+    __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
-        """ set attribute method  """
-        if name == 'first_name':
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError(f"'LockedClass' object has \
-no attribute '{name}'")
+    def __init__(self):
+        """Init method"""
+        pass
