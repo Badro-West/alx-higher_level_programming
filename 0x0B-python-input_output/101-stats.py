d80cad22a95f13773b476df90aebb8c151f445af
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-11-07 18:32:14 -0800

    Directory: 0x0B-python-input_output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-'''Module for log parsing script.'''
 import sys
 
-if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+def print_stats(file_sizes, status_codes):
+    print("File size: {}".format(sum(file_sizes)))
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
-    def check_match(line):
-        '''Checks for regexp match in line.'''
+file_sizes = []
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+
+try:
+    for line in sys.stdin:
         try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except:
+            parts = line.split()
+            file_size = int(parts[-1])
+            status_code = int(parts[-2])
+
+            file_sizes.append(file_size)
+            status_codes[status_code] += 1
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats(file_sizes, status_codes)
+
+        except (ValueError, IndexError):
             pass
 
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+except KeyboardInterrupt:
+    pass
+
+print_stats(file_sizes, status_codes)
+
